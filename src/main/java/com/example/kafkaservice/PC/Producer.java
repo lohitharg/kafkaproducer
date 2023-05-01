8ae24bb4ab63a6e37064762566ecefb5e6c83164
lohit <lohitharg16@gmail.com>
2023-05-01 13:25:14 -0400

    producer

--- src/main/java/com/example/kafkaservice/PC/Producer.java
+++ src/main/java/com/example/kafkaservice/PC/Producer.java
@@ -4,14 +4,14 @@ package com.example.kafkaservice.PC;
 import com.example.kafkaservice.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Producer {
 
     // Autowiring Kafka Template
-    @Autowired KafkaTemplate<String, Employee> kafkaTemplate;
+    @Autowired
+    KafkaTemplate<String, Employee> kafkaTemplate;
 
 
     public void sendMessage(String message) {
